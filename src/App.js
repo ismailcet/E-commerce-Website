6c422f993798806dd13591a1c28039adcf3f3829
ismailcet <ismailcet3@gmail.com>
2022-09-06 18:58:42 +0300

    E-commerce Starter Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import SHOP_DATA from "./shop_data";
 
 function App() {
+  console.log(SHOP_DATA);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      deneme
+      {SHOP_DATA.map((cat) =>
+        cat.items.map((item) => (
+          <img src={require(`${item.header_img}`)} alt="" />
+        ))
+      )}
     </div>
   );
 }
