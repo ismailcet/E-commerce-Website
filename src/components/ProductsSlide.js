ad96cca22ab4a6b7a839b31d089457482f442fc5
ismailcet <ismailcet3@gmail.com>
2022-09-08 00:03:55 +0300

    Slider added in Home page

--- src/components/ProductsSlide.js
+++ src/components/ProductsSlide.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 const ProductsSlide = () => {
-  return <div>ProductsSlide</div>;
+  return (
+    <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default ProductsSlide;
