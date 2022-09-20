b5c4dfc7c17e9e1377bfe12d15230093a07bc6f4
ismailcet <ismailcet3@gmail.com>
2022-09-20 14:59:59 +0300

    Toggle Navbar is added in Project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../Images/Logo.svg";
 import ProfilePhoto from "../Images/user.png";
 import Cart from "../Images/Cart.svg";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <header className="header">
-      <nav>
-        <div className="nav-logo">
-          <Link to="/">
-            <img src={Logo} alt="Logo" className="nav_logo" />
-          </Link>
-        </div>
-        <ul className="nav-items">
-          <li className="hamburger-icon">
-            <GiHamburgerMenu />
-          </li>
+    <>
+      <header className="header">
+        <nav>
+          <div className="nav-logo">
+            <Link to="/">
+              <img src={Logo} alt="Logo" className="nav_logo" />
+            </Link>
+          </div>
+          <ul className="nav-items">
+            <a className="hamburger-icon" onClick={toggleNav}>
+              {isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+            </a>
 
-          <ul className="nav-links">
-            <li className="nav-item">
-              <Link to="/products/female">Female</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/products/male">Male</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/products/kids">Kids</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/products/popular">Popular Product</Link>
-            </li>
+            <ul className="nav-links">
+              <li className="nav-item">
+                <Link to="/products/female">Female</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/products/male">Male</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/products/kids">Kids</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/products/popular">Popular Product</Link>
+              </li>
+            </ul>
           </ul>
-        </ul>
-      </nav>
-      <div className="left_side">
-        <div className="profile">
-          <p className="profile_text">
-            <span className="hello">Hello</span>
-            <span className="name">Eda Bilgili</span>
-          </p>
-          <div className="profiles_img">
-            <img src={ProfilePhoto} alt="User Photo" className="user_img" />
-            <img src={Cart} alt="Cart Img" className="cart_img" />
+        </nav>
+        <div className="left_side">
+          <div className="profile">
+            <p className="profile_text">
+              <span className="hello">Hello</span>
+              <span className="name">Eda Bilgili</span>
+            </p>
+            <div className="profiles_img">
+              <img src={ProfilePhoto} alt="User Photo" className="user_img" />
+              <img src={Cart} alt="Cart Img" className="cart_img" />
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <ul className={`menu-items ${isOpen && "open-items"}`}>
+        <li className="nav-item">
+          <Link to="/products/female">Female</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/products/male">Male</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/products/kids">Kids</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/products/popular">Popular Product</Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
